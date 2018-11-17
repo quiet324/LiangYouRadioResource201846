f9a53727f8b701b64a4481f29c6ab7e3836e19db
Paul <510830568@qq.com>
2018-11-17 03:30:05 -0500

    Auto-commit

--- node/everyday/all-week-after-week-cos-and-qiniu-do-not-upload.js
+++ node/everyday/all-week-after-week-cos-and-qiniu-do-not-upload.js
@@ -16,7 +16,7 @@ var deasync = require('deasync');
 var rule = new schedule.RecurrenceRule();
 // rule.dayOfWeek = [0, new schedule.Range(4, 6)];
 rule.hour =  [1, 2,  6, 9, 16, 18, 21];
-rule.minute = 16;
+rule.minute = 30;
 
 
 
@@ -219,7 +219,7 @@ var j = schedule.scheduleJob(rule, function() { // rule hour at 5 minutes
                         console.log(moment().format('MMMM Do YYYY, h:mm:ss a') + artist.name + audio.time)
                             // var today = dateFormat(new Date(), "yyyymmdd");
                         var today = moment().format("YYYYMMDD");
-                        var yesterday = moment().add(-4, 'days').format("YYYYMMDD");
+                        var yesterday = moment().add(-3, 'days').format("YYYYMMDD");
 
                        //if (audio.time === today || (audio.time === yesterday && artist.id === 15)) {
                             if (audio.time === yesterday) {
